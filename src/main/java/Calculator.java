173edc1dbec8d69b4fb3a72c3008f9ef5b4f53a8
hfzeng <hfzeng@foxmail.com>
2022-04-26 20:45:22 +0800

    refactor calculator code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,12 +1,6 @@
 public class Calculator {
     public int compute(int num1, int num2, char symbol) throws IllegalAccessException {
-        switch (symbol) {
-            case '+':
-                return num1 + num2;
-            case '-':
-                return num1 - num2;
-            default:
-                throw new IllegalAccessException();
-        }
+        Computable computable = ComputeFactory.getCompute(symbol);
+        return computable.compute(num1, num2);
     }
 }
